3a17e1dff3a3a4e7c1d0f958d3fed1e1e20accb0
James La Novara-Gsell <james.lanovara.gsell@gmail.com>
2025-06-03 13:54:28 -0400

    Adds no-neck-pain and nvim-surround

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,9 +1,9 @@
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 vim.o.number = true
 vim.o.relativenumber = true
 
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
@@ -12,7 +12,7 @@ vim.o.updatetime = 300
 
 vim.o.termguicolors = true
 
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 vim.opt.smartindent = true
 
@@ -37,13 +37,15 @@ vim.opt.undofile = true
 
 vim.opt.list = true -- show whitespace
 local space = "·"
-vim.opt.listchars:append {
+vim.opt.listchars:append({
     tab = "│─",
     multispace = space,
     lead = space,
     trail = space,
-    nbsp = space
-}
+    nbsp = space,
+})
 
 vim.opt.cursorline = true
--- vim.opt.hlsearch = true -- higlight search matches as you type
+vim.opt.hlsearch = true -- higlight search matches as you type
+
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
