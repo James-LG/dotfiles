01f64766d434e82c75bd1b000ae3efc1b4fe4d08
James La Novara-Gsell <james.lanovara.gsell@gmail.com>
2025-09-17 11:46:28 -0400

    feat(nvim): Add remaps to move selected text

--- packages/nvim/.config/nvim/lua/config/remap.lua
+++ packages/nvim/.config/nvim/lua/config/remap.lua
@@ -1,3 +1,8 @@
 vim.g.mapleader = " "
 -- commented out while using nvim-tree
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+--
+
+-- Move selected line / block of text in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
