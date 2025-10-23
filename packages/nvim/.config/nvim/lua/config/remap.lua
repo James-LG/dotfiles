124c0c16416dbecf69efbd5d38ea31e4cabce47d
James La Novara-Gsell <james.lanovara.gsell@gmail.com>
2025-10-23 11:01:30 -0400

    feat(nvim): Make diagnostics window focusable

--- packages/nvim/.config/nvim/lua/config/remap.lua
+++ packages/nvim/.config/nvim/lua/config/remap.lua
@@ -6,3 +6,8 @@ vim.g.mapleader = " "
 -- Move selected line / block of text in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Open focusaable diagnostics window
+vim.keymap.set("n", "<C-w>d", function()
+    vim.diagnostic.open_float({ focusable = true })
+end, { desc = "Open Diagnostics Float" })
