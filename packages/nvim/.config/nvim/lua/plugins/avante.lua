78b867407b9c9a19051c3f093987ab64827ff9f7
James La Novara-Gsell <james.lanovara.gsell@gmail.com>
2025-11-26 11:06:19 -0500

    feat(nvim): Configure markdown headings

--- packages/nvim/.config/nvim/lua/plugins/avante.lua
+++ packages/nvim/.config/nvim/lua/plugins/avante.lua
@@ -12,37 +12,10 @@ return {
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
         --- The below dependencies are optional,
-        "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
+        "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
         "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-        "zbirenbaum/copilot.lua",      -- for providers='copilot'
-        {
-            -- support for image pasting
-            "HakonHarnes/img-clip.nvim",
-            event = "VeryLazy",
-            opts = {
-                -- recommended settings
-                default = {
-                    embed_image_as_base64 = false,
-                    prompt_for_file_name = false,
-                    drag_and_drop = {
-                        insert_mode = true,
-                    },
-                    -- required for Windows users
-                    use_absolute_path = true,
-                },
-            },
-        },
-        {
-            -- Make sure to set this up properly if you have lazy=true
-            'MeanderingProgrammer/render-markdown.nvim',
-            opts = {
-                file_types = { "markdown", "Avante" },
-                code = {
-                    style = 'full',
-                    border = 'thin'
-                }
-            },
-            ft = { "markdown", "Avante" },
-        },
+        "zbirenbaum/copilot.lua", -- for providers='copilot'
+        "MeanderingProgrammer/render-markdown.nvim", -- markdown rendering
+        "HakonHarnes/img-clip.nvim", -- image pasting
     },
 }
