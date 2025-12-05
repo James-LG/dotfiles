c1f16a26b1f0302a7d1edd36d23ab49ede82fd06
James La Novara-Gsell <james.lanovara.gsell@gmail.com>
2025-12-05 10:37:21 -0500

    feat(nvim): Update avante config

--- packages/nvim/.config/nvim/lua/plugins/avante.lua
+++ packages/nvim/.config/nvim/lua/plugins/avante.lua
@@ -15,12 +15,43 @@ return {
         "MeanderingProgrammer/render-markdown.nvim", -- markdown rendering
         "HakonHarnes/img-clip.nvim", -- image pasting
     },
+    keys = {
+        {
+            "<leader>a+",
+            function()
+                local tree_ext = require("avante.extensions.nvim_tree")
+                tree_ext.add_file()
+            end,
+            desc = "Select file in NvimTree",
+            ft = "NvimTree",
+        },
+        {
+            "<leader>a-",
+            function()
+                local tree_ext = require("avante.extensions.nvim_tree")
+                tree_ext.remove_file()
+            end,
+            desc = "Deselect file in NvimTree",
+            ft = "NvimTree",
+        },
+    },
     config = function()
         local ai_provider = os.getenv("AVANTE_AI_PROVIDER") or "copilot"
 
         require("avante").setup({
+            debug = false,
             provider = ai_provider,
-            -- other shared avante settings
+            providers = {
+                gemini = {
+                    model = "gemini-2.5-flash",
+                },
+                copilot = {
+                    model = "gpt-4o",
+                },
+            },
+            web_search_engine = {
+                provider = "google",
+            },
         })
     end,
 }
